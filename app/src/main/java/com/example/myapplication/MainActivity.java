5a5ed277b59809a9c2c445d4515a38bd61674346
Oussama CHALLAL <Ochal073@uottawa.ca>
2020-10-20 03:45:55 +0100

    add missing features

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,26 +12,80 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etTeamAddress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etTeamAddress = findViewById(R.id.edtAdress);
+
     }
 
-    public void OnOpenInGoogleMaps(View view){
-        EditText teamAddres = (EditText)findViewById(R.id.teamAddressField);
 
-        Uri gmmIntentUri = Uri.parse("http://maps.google.co.in/maps?q="+ teamAddres.getText());
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageView avatarImage = findViewById(R.id.avatarImage);
 
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+        Log.d("MAIN ACTIVITY", String.format("Result code : %d",resultCode));
+        assert data != null;
+        if (resultCode == 33) {
+            String currentPhotoPath = data.getStringExtra("imageUrl");
+            Bitmap photo = BitmapFactory.decodeFile(currentPhotoPath);
+            photo = Bitmap.createScaledBitmap(photo, 300, 300, false);
+            avatarImage.setImageBitmap(photo);
+        } else {
+            String drawableName;
+            switch (data.getIntExtra("imageID", R.id.teamid00)) {
+                case R.id.teamid00:
+                    drawableName = "ic_logo_00";
+                    break;
+                case R.id.teamid01:
+                    drawableName = "ic_logo_01";
+                    break;
+                case R.id.teamid02:
+                    drawableName = "ic_logo_02";
+                    break;
+                case R.id.teamid03:
+                    drawableName = "ic_logo_03";
+                    break;
+                case R.id.teamid04:
+                    drawableName = "ic_logo_04";
+                    break;
+                case R.id.teamid05:
+                    drawableName = "ic_logo_05";
+                    break;
+                default:
+                    drawableName = "ic_logo_00";
+                    break;
+            }
+            int resourceId = getResources().getIdentifier(drawableName, "drawable", getPackageName());
+            avatarImage.setImageResource(resourceId);
+        }
+
+    }
+
+    public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+    public void onOpenInGoogleMaps(View view) {
+        String teamAddress = etTeamAddress.getText().toString();
+
+        if (teamAddress.isEmpty()) {
+            etTeamAddress.setError("Please provide a valid address!");
+            return;
+        }
+
+        Uri gmmIntentUri = Uri.parse(String.format("http://maps.google.co.in/maps?q=%s", teamAddress));
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
-
         mapIntent.setPackage("com.google.android.apps.maps");
-
         startActivity(mapIntent);
     }
 
-    public void OnSetAvatarButton(View view){
-       Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
-        startActivityForResult(intent,0);
-    }
 }
--- app/src/main/java/com/example/myapplication/ProfileActivity.java
+++ app/src/main/java/com/example/myapplication/ProfileActivity.java
@@ -6,9 +6,132 @@ import android.os.Bundle;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE = 2;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private static final int MY_STORAGE_REQUEST_CODE = 101;
+    private String currentPhotoPath;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void setTeamIcon(View view) {
+        Intent returnIntent = new Intent();
+        ImageView selectedImageView = (ImageView) view;
+        if (selectedImageView.getId() == R.id.loadedImage) {
+            returnIntent.putExtra("imageUrl", currentPhotoPath);
+            setResult(33, returnIntent);
+        } else {
+            returnIntent.putExtra("imageID", selectedImageView.getId());
+            setResult(RESULT_OK, returnIntent);
+        }
+        finish();
+    }
+
+    public void loadFromPhone(View view) {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    },
+                    MY_STORAGE_REQUEST_CODE);
+        } else {
+            Intent takePictureIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE);
+        }
+    }
+
+    public void openCamera(View view) {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        } else {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            // Ensure that there's a camera activity to handle the intent
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                // Create the File where the photo should go
+                File photoFile = null;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException ex) {
+                    // Error occurred while creating the File
+                    ex.printStackTrace();
+                }
+                // Continue only if the File was successfully created
+                if (photoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(this,
+                            "ca.challal.lab04.fileprovider",
+                            photoFile);
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+        }
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_REQUEST_CODE || requestCode == MY_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != Activity.RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_CAPTURE) {
+                setCurrentImage();
+            } else if (requestCode == REQUEST_IMAGE) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(selectedImage,
+                        filePathColumn,
+                        null,
+                        null,
+                        null);
+                cursor.moveToFirst();
+                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                String picturePath = cursor.getString(columnIndex);
+                this.currentPhotoPath = picturePath;
+                cursor.close();
+                setCurrentImage();
+            }
+        }
+    }
+
+    private void setCurrentImage() {
+        ImageView imageView = (ImageView) findViewById(R.id.loadedImage);
+        Bitmap photo = BitmapFactory.decodeFile(currentPhotoPath);
+        photo = Bitmap.createScaledBitmap(photo, 300, 300, false);
+        imageView.setImageBitmap(photo);
     }
 }
